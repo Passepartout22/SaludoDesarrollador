9dcb1c67f2bada8f23bfbc1791f3513432b16df7
zeus <emmanuelcach6@gmail.com>
2022-02-19 00:01:28 -0500

    Ultima Actualizacion de la aplicacion, finalizado

--- app/src/main/java/com/reeze/miprimeraaplicacion/MainActivity.kt
+++ app/src/main/java/com/reeze/miprimeraaplicacion/MainActivity.kt
@@ -2,6 +2,7 @@ package com.reeze.miprimeraaplicacion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
@@ -12,15 +13,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun validaDato(): Boolean {
+        val bSaludar = findViewById<Button>(R.id.btnSaludar)
+        val etNombre = findViewById<EditText>(R.id.etNombre)
+        val cbDev = findViewById<CheckBox>(R.id.cbDevelopers)
 
-            val etNombre = findViewById<EditText>(R.id.etNombre)
-            val nombreUsuario = etNombre.text
-
-            if(nombreUsuario.isNullOrEmpty()) {
-                return false
+        bSaludar.setOnClickListener(View.OnClickListener {
+            if(validaDato()) {
+                if(cbDev. isChecked) {
+                    Toast.makeText(this, "Bienvenido $etNombre se que eres un desarrollador", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Bienvenido $etNombre", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(this, "Escribe tu nombre para saludarte :/", Toast.LENGTH_SHORT).show()
             }
-            return true;
+        })
+    }
+    fun validaDato(): Boolean {
+
+        val etNombre = findViewById<EditText>(R.id.etNombre)
+        val nombreUsuario = etNombre.text
+
+        if(nombreUsuario.isNullOrEmpty()) {
+            return false
         }
+        return true;
     }
 }
